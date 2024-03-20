e3b48e2ac9b76a1c1fd2862f18c95eba7b116c90
Truong Pham <truongpham@Truongs-MacBook.local>
2024-03-20 14:28:03 +0700

    create tournament

--- client/src/pages/components/layouts/main.jsx
+++ client/src/pages/components/layouts/main.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { Footer, Navbar } from '../..'
-import { useSelector } from 'react-redux';
-import Home from '../../home'
-import League from '../../league'
-import Tournament from '../../tournament'
-import Team from '../../team'
+import React from "react";
+import { Footer, Navbar } from "../..";
+import { useSelector } from "react-redux";
+import Home from "../../home";
+import League from "../../league";
+import CreateTournament from "../../tournament/CreateTournament";
+import FindTournament from "../../tournament/FindTournament";
+import Team from "../../team";
 const Main = () => {
-  const layout = useSelector((state)=> state.ui.view)
-  console.log('====================================');
-  console.log(layout);
-  console.log('====================================');
+  const layout = useSelector((state) => state.ui.view);
   return (
     <div>
-        <Navbar />  
-        {layout === 'homeClient' && <Home />}
-        {layout === 'league' && <League />}
-        {layout === 'tournament' && <Tournament />}
-        {layout === 'team' && <Team />}
-        <Footer />
+      <Navbar />
+      <div className="max-w-7xl px-2 sm:px-6 lg:px-8 mt-10 mx-auto">
+      {layout === "homeClient" && <Home />}
+      {layout === "league" && <League />}
+      {layout === "CreateTournament" && <CreateTournament />}
+      {layout === "FindTournament" && <FindTournament />}
+      {layout === "team" && <Team />}</div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
