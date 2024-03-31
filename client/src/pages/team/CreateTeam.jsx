ee654cfa7abe8e5007dbc99a0294c115f2a53234
Nguyen Thang <thangnguyen10012003@gmail.com>
2024-03-31 11:22:22 +0700

    fix

--- client/src/pages/team/CreateTeam.jsx
+++ client/src/pages/team/CreateTeam.jsx
@@ -7,15 +7,6 @@ import {
   Select,
   TextField,
 } from "@mui/material";
-<<<<<<< HEAD
-
-const CreateTeam = () => {
-  const [policy, setPolicy] = useState("");
-  const [winPoint, setwinPoint] = useState(3);
-  const [breakEvenPoint, setbreakEvenPoint] = useState(1);
-  const [lossPoint, setlossPoint] = useState(0);
-  const [amountPerTeam, setamountPerTeam] = useState(5);
-=======
 import logo from "../../assets/logo/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
@@ -26,21 +17,10 @@ const CreateTeam = () => {
   // const [breakEvenPoint, setbreakEvenPoint] = useState(1);
   // const [lossPoint, setlossPoint] = useState(0);
   // const [amountPerTeam, setamountPerTeam] = useState(5);
->>>>>>> dcce4c59056ba13d9cea71cbdbfb3e5868fd54bf
 
   const handleChangePolicy = (event) => {
     setPolicy(event.target.value);
   };
-<<<<<<< HEAD
-  const handleamountPerTeam = (event) => {
-    setamountPerTeam(event.target.value);
-  };
-  return (
-    <div className="min-h-[1000px] p-24">
-      <div className="flex items-center justify-between">
-        <div>
-          <h2 className="text-4xl font-bold">Tạo đội</h2>
-=======
   // const handleamountPerTeam = (event) => {
   //   setamountPerTeam(event.target.value);
   // };
@@ -49,7 +29,6 @@ const CreateTeam = () => {
       <div className="flex items-center justify-between">
         <div>
           <h2 className="text-4xl font-bold">Tạo đội đấu</h2>
->>>>>>> dcce4c59056ba13d9cea71cbdbfb3e5868fd54bf
           <p className="my-3">
             Vui lòng nhập thông tin hợp lệ cho các trường được yêu cầu{" "}
             <span className="text-red-500">*</span>
@@ -63,147 +42,6 @@ const CreateTeam = () => {
         </div>
       </div>
       <Divider />
-<<<<<<< HEAD
-      <div className="my-10 flex justify-between items-center">
-        <div className="mt-0 mx-auto w-1/5 flex flex-col items-centererit">
-          <img
-            src="https://www.topgear.com/sites/default/files/2023/09/33136-RS7PERFORMANCEASCARIBLUEJORDANBUTTERS208.jpg?w=1784&h=1004"
-            alt="logo tournament"
-            className="w-56 h-56 object-cover"
-          />
-        </div>
-        <div className="w-2/3 flex  flex-col gap-3">
-          <TextField
-            id="standard-basic"
-            label="Tên đội"
-            variant="standard"
-            className="mt-3"
-          />
-          <FormControl sx={{ minWidth: 120, mt: 3 }} size="small">
-            <InputLabel id="demo-select-small-label">Trình độ</InputLabel>
-            <Select
-              labelId="demo-select-small-label"
-              id="demo-select-small"
-              value={policy}
-              label="Trình độ"
-              onChange={handleChangePolicy}
-            >
-              <MenuItem value={"ChuyenNghiep"}>Chuyên nghiệp</MenuItem>
-              <MenuItem value={"BanChuyen"}>Bán chuyên</MenuItem>
-              <MenuItem value={"CaoCap"}>Cao cấp</MenuItem>
-              <MenuItem value={"TrungCap"}>Trung cấp</MenuItem>
-              <MenuItem value={"Vui"}>Vui</MenuItem>
-              <MenuItem value={"Khac"}>Khác</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl sx={{ minWidth: 120, mt: 3 }} size="small">
-            <InputLabel id="demo-select-small-label">Độ tuổi</InputLabel>
-            <Select
-              labelId="demo-select-small-label"
-              id="demo-select-small"
-              value={policy}
-              label="Độ tuổi"
-              onChange={handleChangePolicy}
-            >
-              <MenuItem value={"15"}>15-20</MenuItem>
-              <MenuItem value={"20"}>20-25</MenuItem>
-              <MenuItem value={"25"}>25-30</MenuItem>
-              <MenuItem value={"30"}>&gt; 30</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl sx={{ minWidth: 120, mt: 3 }} size="small">
-            <InputLabel id="demo-select-small-label">Môn đấu</InputLabel>
-            <Select
-              labelId="demo-select-small-label"
-              id="demo-select-small"
-              value={policy}
-              label="Môn đấu"
-              onChange={handleChangePolicy}
-            >
-              <MenuItem value={"AOE"}>AOE</MenuItem>
-              <MenuItem value={"Bida"}>Bida</MenuItem>
-              <MenuItem value={"BongBan"}>Bóng bàn</MenuItem>
-              <MenuItem value={"BongChuyen"}>Bóng chuyền</MenuItem>
-              <MenuItem value={"BongRo"}>Bóng rổ</MenuItem>
-              <MenuItem value={"BongDa"}>Bóng đá</MenuItem>
-            </Select>
-          </FormControl>
-
-          <div className="my-5">
-            <div className="flex justify-between gap-10">
-              <div className="flex flex-col w-1/2">
-                <TextField
-                  id="phone"
-                  label="Số điện thoại"
-                  variant="standard"
-                  className="mt-3"
-                />
-              </div>
-              <div className="flex flex-col w-1/3">
-                <FormControl sx={{ minWidth: 120, mt: 3}} size="small">
-                    <InputLabel id="demo-select-small-label">Chế độ</InputLabel>
-                    <Select
-                      labelId="demo-select-small-label"
-                      id="demo-select-small"
-                      value={policy}
-                      label="Chế độ"
-                      onChange={handleChangePolicy}
-                    >
-                      <MenuItem value={"private"}>Riêng tư</MenuItem>
-                      <MenuItem value={"public"}>Công khai</MenuItem>
-                    </Select>
-                </FormControl>
-              </div>
-            </div>
-          </div>
-          <div className="my-5">
-            <div className="flex justify-between gap-10">
-              <div className="flex flex-col w-1/2">
-                <TextField
-                  id="email"
-                  label="Email"
-                  variant="standard"
-                  className="mt-3"
-                />
-              </div>
-              <div className="flex flex-col w-1/3">
-                <FormControl sx={{ minWidth: 120, mt: 3}} size="small">
-                    <InputLabel id="demo-select-small-label">Chế độ</InputLabel>
-                    <Select
-                      labelId="demo-select-small-label"
-                      id="demo-select-small"
-                      value={policy}
-                      label="Chế độ"
-                      onChange={handleChangePolicy}
-                    >
-                      <MenuItem value={"private"}>Riêng tư</MenuItem>
-                      <MenuItem value={"public"}>Công khai</MenuItem>
-                    </Select>
-                </FormControl>
-              </div>
-            </div>
-          </div>  
-          <div className="my-5">
-            <div className="flex justify-between gap-10">
-              <div className="flex flex-col w-1/2">
-                <TextField
-                  id="area"
-                  label="Khu vực hoạt động(Tùy chọn)"
-                  variant="standard"
-                  className="mt-3"
-                />
-              </div>
-
-            </div>
-          </div>  
-
-
-        </div>
-      </div>
-      <Divider />
-      <div className="my-10 flex justify-center items-center ">
-        <button className="bg-gray-500 text-white rounded-md py-3 px-8 hover:bg-red-500 transition">Tạo đội</button>
-=======
 
       {
         // thông tin đội
@@ -342,14 +180,9 @@ const CreateTeam = () => {
         <button className="bg-gray-500 text-white rounded-md py-3 px-8 hover:bg-red-500 transition">
           Tạo đội
         </button>
->>>>>>> dcce4c59056ba13d9cea71cbdbfb3e5868fd54bf
       </div>
     </div>
   );
 };
 
-<<<<<<< HEAD
 export default CreateTeam;
-=======
-export default CreateTeam;
->>>>>>> dcce4c59056ba13d9cea71cbdbfb3e5868fd54bf
--- client/src/pages/team/FindTeam.jsx
+++ client/src/pages/team/FindTeam.jsx
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import {CardActionArea, CardActions } from '@mui/material';
-const index = () => {
-  return (
-    <div className="min-h-[1000px] p-24 bg-gray-300 ">
-      <div className="flex flex-col items-center justify-between gap-5" style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))' }}>
-      <Card sx={{ maxWidth: 345}}>
-      <CardActionArea className='flex flex-col items-center justify-between'>
-        <CardMedia
-          component="img"
-          image={"https://www.topgear.com/sites/default/files/2023/09/33136-RS7PERFORMANCEASCARIBLUEJORDANBUTTERS208.jpg?w=1784&h=1004"}
-          className='p-5'
-          alt="team logo"
-          style={{ objectFit: 'cover', width: '100%', height: '200px' }} // Ensure image fills container
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div" style={{ textAlign: 'center' ,color:'green',fontWeight:'bold'}}>
-            team logo
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions className='flex flex-col'>
-        <div className='mx-auto'>Thành viên</div>
-        <img src={"https://www.topgear.com/sites/default/files/2023/09/33136-RS7PERFORMANCEASCARIBLUEJORDANBUTTERS208.jpg?w=1784&h=1004"} alt="member" className='w-10 h-10 rounded-full mx-auto' /> {/* Adjust image size and style */}
-      </CardActions>
-    </Card>
-
-
-
-
-        
-
-
-      </div>
-    </div>
-
-
-  )
-}
-
-export default index
-=======
 import React, { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
@@ -142,4 +95,3 @@ const FindTeam = () => {
 };
 
 export default FindTeam;
->>>>>>> dcce4c59056ba13d9cea71cbdbfb3e5868fd54bf
